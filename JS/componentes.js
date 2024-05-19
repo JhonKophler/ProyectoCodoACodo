6959840624026ab856cde4e41655070076765221
jhonkophler <juanadriancorso94@gmail.com>
2024-05-19 20:57:17 -0300

    modal, mas la carga de los datos del form al json

--- JS/componentes.js
+++ JS/componentes.js
@@ -62,7 +62,7 @@ function mostrarTickets(tickets) {
     var ticketCard = `
       <div class="card-nb" data-id="${ticket.id}">
           <div class="card-header-nb">
-            <p id="t-prioridad" style="color: ${ticket.colorPrioridad};font-size:1.2rem;text-transform: capitalize;">${ticket.prioridad}</p>
+            <p id="t-prioridad" style="color: ${ticket.colorPrioridad};font-size:1.2rem;text-transform: capitalize;font-weight: 700;">${ticket.prioridad}</p>
             <p id="t-id">Nº ${ticket.id}</p>
           </div>
           <div class="card-body-nb">
@@ -86,16 +86,16 @@ function enviarAWebTicket() {
     // Agregamos el evento de clic al contenedor de tickets.
     ticketsContainer.addEventListener("click", (event) => {
       if (event.target.classList.contains("cargarHtml")) {
-        const ticketId = event.target.getAttribute("data-id");
-        window.location.href = `/section/ticket-detail.html?id=${ticketId}`;
+        const varTicketId = event.target.getAttribute("data-id");
+        window.location.href = `/section/ticket-detail.html?id=${varTicketId}`;
       }
     });
   });
 }
 
 //funcion para Cargar "section/ticket-detail.html" con la info del ticket seleccionado
-function cargarTicketsDeleccionado() {
-  document.addEventListener("DOMContentLoaded", (event) => {
+function cargarTicketSeleccionado() {
+  document.addEventListener("DOMContentLoaded", () => {
     const urlParams = new URLSearchParams(window.location.search);
     const ticketId = urlParams.get("id");
 
@@ -107,38 +107,39 @@ function cargarTicketsDeleccionado() {
           const ticket = tickets.find((t) => t.id == ticketId);
 
           if (ticket) {
-            console.log(ticket);
             const ticketDetails = document.getElementById("details");
             var ticketArmado = `
-              <div class="detail-card">
-                <div class="detail-card-header">
-                    <div class="cajaAlineadoraCabecera">
+            <div class="detail-card">
+              <div class="detail-card-header">
+                  <div class="cajaAlineadoraCabecera">
                       <p style="text-transform: capitalize;"><strong>Prioridad</strong></p>
                       ${ticket.prioridad}
-                    </div>
-                    <div class="cajaAlineadoraCabecera">
+                  </div>
+                  <div class="cajaAlineadoraCabecera">
                       <p><strong>Ticket</strong></p>
                       Nº ${ticket.id}
-                    </div>
-                    <div class="cajaAlineadoraCabecera">
+                  </div>
+                  <div class="cajaAlineadoraCabecera">
                       <p><strong>Usuario</strong></p>
                       ${ticket.user}
-                    </div>
-                    
-                </div>
-                <hr class="border border-primary border-3 opacity-75 w-10">
-                <div class="detail-card-body">
-                    <h2><strong>${ticket.titulo}</strong></h2>
-                    <p><strong>Detalle:</strong> ${ticket.detalle}</p>
-                </div>
-                <hr class="border border-primary border-3 opacity-75 w-10">
-                <div class="botonesAcciones">
-                  <button onClick=window.history.back(); type="button" class="btn btn-dark">Volver</button>
-                  <aside>
-                    <button type="button" class="btn btn-success">Resolver</button>
-                    <button type="button" class="btn btn-danger">Cancelar</button>
-                  </aside>
-                </div>
+                  </div>
+              </div>
+              <hr class="border border-primary border-3 opacity-75 w-10" style="margin-bottom:0!important;">
+              <div class="fechaCarga"><p>Creado ${formatearFecha(
+                ticket.fechaCarga
+              )}</p></div>
+              <div class="detail-card-body">
+                  <h5><strong>${ticket.titulo}</strong></h5>
+                  <p><strong>Detalle</strong><br> ${ticket.detalle}</p>
+                  <hr class="border border-primary border-3 opacity-75 w-10">
+                  <div class="botonesAcciones">
+                      <button onClick=window.history.back(); type="button" class="btn btn-dark">Volver</button>
+                      <aside>
+                          <button type="button" class="btn btn-success">Resolver</button>
+                          <button type="button" class="btn btn-danger">Cancelar</button>
+                      </aside>
+                  </div>
+              </div>
             </div>`;
             ticketDetails.innerHTML = ticketArmado;
           } else {
@@ -152,4 +153,68 @@ function cargarTicketsDeleccionado() {
       console.error("ID del ticket no especificado en la URL");
     }
   });
+}
+
+function formatearFecha(fechaString) {
+  // Reemplazamos el espacio por 'T' para que el constructor Date lo entienda
+  let fechaISO = fechaString.replace(" ", "T");
+
+  // Creamos un objeto Date
+  let fecha = new Date(fechaISO);
+
+  // Configuramos las opciones para formatear la fecha
+  let opciones = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true, // Para usar formato de 12 horas (AM/PM)
+  };
+
+  // Utilizamos Intl.DateTimeFormat para formatear la fecha
+  let fechaFormateada = new Intl.DateTimeFormat("es-ES", opciones).format(
+    fecha
+  );
+
+  return fechaFormateada;
+}
+
+function obtenerFechaActual() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const fechaElement = document.getElementById("fechaActual");
+    const fecha = new Date();
+    const opciones = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    };
+    const fechaFormateada = fecha.toLocaleDateString("es-ES", opciones);
+    fechaElement.textContent = fechaFormateada;
+  });
+}
+
+//Espera una lista de tickets asi los acomoda y trae el ultimo primero
+function insertarUltimoIdTicket(tickets) {
+  // Ordenamos con SORT
+  tickets.sort((a, b) => new Date(b.fechaCarga) - new Date(a.fechaCarga));
+
+  const divIdTicketElement = document.getElementById("idTicketModal");
+  divIdTicketElement.innerHTML = tickets[0].id + 1;
+} 
+
+function generarUserID() {
+  return crypto.randomUUID();
+}
+
+function verificarUserID() {
+  let userUUID = localStorage.getItem("userUUID");
+  if (!userUUID) {
+    userUUID = generateUUID();
+    localStorage.setItem("userUUID", userUUID);
+  }
+  return userUUID;
 }
