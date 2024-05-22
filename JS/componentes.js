a1867426709a8e47f8105567f419a5c6b51f09ba
Angeles Licudis <angeleslicudis@gmail.com>
2024-05-22 19:28:11 -0300

    Borré el archivo de js para unificar todo en uno

--- JS/componentes.js
+++ JS/componentes.js
@@ -326,3 +326,32 @@ function verificarUserID() {
 //     form.submit();
 //   });
 // }
+
+
+
+
+
+
+
+
+
+//Aqui empieza el slide de comentarios //
+
+const initSlider = () => {
+  const caja = document.querySelector(".caja-contenedor .caja")
+
+  const slideButton = document.querySelector(".caja-contenedor .slide-button")
+
+  //mueve los comentarios según las flechas
+
+  slideButton.forEach(button => {
+      button.addEventListener("click", () =>{
+          const direction  = button.id === "atras" ? -1 : 1;
+          const scrollAmount = caja.clientWith * direction;
+          caja.scrollBy({left : scrollAmount, behavior: "smooth"})
+      })
+  });
+}
+
+
+window.addEventListener("load", initSlider)
